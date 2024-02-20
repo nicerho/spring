8bad091f6f3030dbeaabbd03572683e25ca7d848
Jung ho Jeon <wjswjdgh03@gmail.com>
2024-02-20 22:21:37 +0900

    ss

--- itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
